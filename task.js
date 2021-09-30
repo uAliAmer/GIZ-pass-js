67b64991a4d5ddae8aad5165019e81d53e4d7228
ualiamer <ali.amur88@gmail.com>
2021-09-30 14:12:44 +0300

    added TODO list

--- task.js
+++ task.js
@@ -31,4 +31,14 @@ for(let i = 0 ; i < num.length ; i++)
 
 console.log("Numbers list After Desc sorting: ", num)
 
+//TODO: Try to figure out the issue of the sorting algorithm, and fix it to work as expected. 
+// Done
 
+//TODO: Change the code to be well-designed and well-styled.
+// Done 
+
+//TODO: The “numbers” array in the code is mutable!, change a little bit in the code to keep the original one as-it (immutable) and print the new one.
+// Done
+
+//TODO: Read the numbers list from data.txt file, sort it in ASC order, then store it in a new file called output.txt
+// Working on it...
