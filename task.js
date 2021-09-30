4c94d79c873edc1a5f910c474b099307c97202d2
ualiamer <ali.amur88@gmail.com>
2021-09-30 13:59:04 +0300

    made the numbers array immutable

--- task.js
+++ task.js
@@ -3,10 +3,10 @@ let num = [5, 8, 0, 1, 9, 11, 5, 16];
 
 console.log("Original numbers list: ", num)
 
-for(var i = 0; i < num.length; i++) {
+for(let i = 0; i < num.length; i++) {
     for(let j=i+1; j < num.length; j++) {
         if(num[i] > num[j]) {
-            var temp = num[i];
+            const temp = num[i];
             num[i] = num[j];
             num[j] = temp;        
         }
@@ -16,13 +16,13 @@ for(var i = 0; i < num.length; i++) {
 console.log("Numbers list after Asc sorting:: ", num);
 
 
-for(var i = 0 ; i < num.length ; i++)
+for(let i = 0 ; i < num.length ; i++)
 {
     for(let j = 0 ; j < num.length - 1 ; j++)
     {
         if(num[i] < num[j + 1])
         {
-            var temp = num[j];
+            const temp = num[j];
             num[j] = num[j + 1]
             num[j + 1] = temp;
         }
